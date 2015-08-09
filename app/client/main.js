4dd1d049e9266a219872e429a1375540d15478ab
Forest Belton <forest@homolo.gy>
2015-08-08 19:29:49 -0700

    Allow dynamic creation of category objects

--- app/client/main.js
+++ app/client/main.js
@@ -2,10 +2,49 @@ import React from 'react';
 import CategoryObject from './components/CategoryObject';
 
 class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            actionState: 'NONE',
+            objects: []
+        };
+    }
+
+    createObject() {
+        this.setState({
+            actionState: 'CREATE_OBJECT'
+        });
+    }
+
+    handleAction(e) {
+        const node = React.findDOMNode(this),
+            x = e.pageX - node.offsetLeft,
+            y = e.pageY - node.offsetTop;
+
+        switch(this.state.actionState) {
+            case 'CREATE_OBJECT':
+                this.setState({
+                    actionState: 'NONE',
+                    objects: this.state.objects.concat(
+                        <CategoryObject x={x} y={y} id="test" name="X" />
+                    )
+                });
+                break;
+        }
+    }
+
     render() {
+        const createObject = this.createObject.bind(this);
+        const handleAction = this.handleAction.bind(this);
+
         return (
-            <div className="board">
-                <CategoryObject x={5} y={5} id="test" name="X" />
+            <div>
+                <div className="command-list">
+                    <button onClick={createObject}>Create object</button>
+                </div>
+                <div className="board" onClick={handleAction}>
+                    {this.state.objects}
+                </div>
             </div>
         );
     }
