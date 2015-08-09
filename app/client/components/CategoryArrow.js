c42393dd33fa800323bbedcec5a994d0f77e81ee
Forest Belton <forest@homolo.gy>
2015-08-09 00:47:36 -0700

    Compute starting point based on angle between two points

--- app/client/components/CategoryArrow.js
+++ app/client/components/CategoryArrow.js
@@ -1,23 +1,35 @@
 import React from 'react';
 
+function startPoint(src, dst) {
+    const angle = Math.atan2(dst.y - src.y, dst.x - src.x);
+
+    return {
+        x: (src.x + 12.5) + 12.5 * Math.cos(angle),
+        y: (src.y + 12.5) + 12.5 * Math.sin(angle)
+    };
+}
+
 export default class CategoryArrow extends React.Component {
     render() {
+        const lineWidth = 4;
+
+        const s0 = startPoint(this.props.src, this.props.dst);
         const arrowStyle = {
-            top: Math.min(this.props.src.y, this.props.dst.y),
-            left: Math.min(this.props.src.x, this.props.dst.x)
+            top: s0.y - lineWidth / 2,
+            left: s0.x - lineWidth / 2
         };
 
-        const height = Math.max(2, Math.abs(this.props.dst.y - this.props.src.y)),
-            width = Math.max(2, Math.abs(this.props.dst.x - this.props.src.x));
+        const height = Math.max(lineWidth, Math.abs(this.props.dst.y - this.props.src.y) - 25),
+            width = Math.max(lineWidth, Math.abs(this.props.dst.x - this.props.src.x) - 25);
 
-        const x1 = this.props.src.x < this.props.dst.x ? 0 : width,
-            y1 = this.props.src.y < this.props.dst.y ? 0 : height,
-            x2 = this.props.src.x < this.props.dst.x ? width : 0,
-            y2 = this.props.src.y < this.props.dst.y ? height : 0;
+        const x1 = this.props.src.x < this.props.dst.x ? 0 : (width == lineWidth ? 0 : width),
+            y1 = this.props.src.y < this.props.dst.y ? 0 : (height == lineWidth ? 0 : height),
+            x2 = this.props.src.x < this.props.dst.x ? (width == lineWidth ? 0 : width) : 0,
+            y2 = this.props.src.y < this.props.dst.y ? (height == lineWidth ? 0 : height) : 0;
 
         return (
             <svg className="arrow" height={height} width={width} style={arrowStyle}>
-                <line x1={x1} y1={y1} x2={x2} y2={y2} strokeWidth="2" stroke="black" />
+                <line x1={x1} y1={y1} x2={x2} y2={y2} strokeWidth={lineWidth} stroke="black" />
             </svg>
         );
     }
